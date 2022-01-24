c02e146fa279849f32ac59b636a918250e84936b
jadoria57 <97111735+jadoria57@users.noreply.github.com>
2022-01-24 15:09:57 +0000

    exercicios 1 e 2

---
+++ src/exerciciosLaboratorio/Ficha2.java
@@ -0,0 +1,44 @@
+package exerciciosLaboratorio;
+
+import java.util.Scanner;
+
+public class Ficha2 
+{
+	public static void main(String[] args) 
+	{
+		Scanner consola = new Scanner(System.in);
+		
+		System.out.println("Indique a altura do triângulo");
+		double alturaTriangulo = consola.nextInt();
+		System.out.println("Indique a base do triângulo");
+		double baseTriangulo = consola.nextInt();
+		double areaTriangulo = areaTriang (alturaTriangulo, baseTriangulo);
+		System.out.println("A área do triângulo é " + areaTriangulo);
+		
+		System.out.println("------------------------------");
+		
+		System.out.println("Indique a altura do retângulo");
+		double alturaRetangulo = consola.nextInt();
+		System.out.println("Indique o comprimento do retângulo");
+		double comprimentoRetangulo = consola.nextInt();
+		double areaRetangulo = areaRetang (alturaRetangulo, comprimentoRetangulo);
+		System.out.println("A área do retângulo é " + areaRetangulo);
+		consola.close();
+		
+		System.out.println("------------------------------");
+	}
+	
+	public static double areaTriang(double aAlturaTriang, double aBaseTriang) 
+	{
+		double area = (aAlturaTriang*aBaseTriang)/2;
+
+		return area;
+	}
+	
+	public static double areaRetang(double aAlturaRetang, double aComprimentoRetang) 
+	{
+		double areaRetang = aAlturaRetang*aComprimentoRetang;
+
+		return areaRetang;
+	}
+}
