4ff2952fc23e9e546f93d73d8f428671e596fbc3
jadoria57 <97111735+jadoria57@users.noreply.github.com>
2022-02-04 10:02:48 +0000

    ficha 2 acabada

--- src/exerciciosLaboratorio/Ficha2.java
+++ src/exerciciosLaboratorio/Ficha2.java
@@ -13,8 +13,7 @@ public class Ficha2
 		System.out.println("Indique a base do triângulo");
 		double baseTriangulo = consola.nextInt();
 		double areaTriangulo = areaTriang (alturaTriangulo, baseTriangulo);
-		System.out.println("A área do triângulo é " + areaTriangulo);
-				
+		System.out.println("A área do triângulo é " + areaTriangulo);	
 		System.out.println("\n");
 				
 		System.out.println("Indique a altura do retângulo");
@@ -23,6 +22,7 @@ public class Ficha2
 		double comprimentoRetangulo = consola.nextInt();
 		double areaRetangulo = areaRetang (alturaRetangulo, comprimentoRetangulo);
 		System.out.println("A área do retângulo é " + areaRetangulo);
+		System.out.println("\n");
 				
 		System.out.println("Indique o valor de z");
 		int z = consola.nextInt();
@@ -31,25 +31,35 @@ public class Ficha2
 		System.out.println("Indique o valor de y");
 		int y = consola.nextInt();
 		double areaCal = areaCal(x, y, z);
-		System.out.println("A área da figura é " + areaCal); 
+		System.out.println("A área da figura é " + areaCal);
+		System.out.println("\n"); 
 				
 		System.out.println("Indique o valor do fatorial n");
 		int n1 = consola.nextInt();
 		double fatorial = factor(n1);
 		System.out.println("O exponencial n1 é: "+ fatorial);
+		System.out.println("\n");
 		
 		System.out.println("Indique o valor do fatorial recursivo de n2");
 		int n2 = consola.nextInt();
 		double fatorialRecursivo = factorRecursivo(n2);
 		System.out.println("O exponencial recursivo de n2 é: "+ fatorialRecursivo);
+		System.out.println("\n");
 		
 		System.out.println("Digite a sua frase:");
         String frase = consola.nextLine().toLowerCase();
         int totalVogais = numTotalVogal(frase);
-        System.out.println("A sua frase possui " + totalVogais + " vogais");*/
+        System.out.println("A sua frase possui " + totalVogais + " vogais");
+        System.out.println("\n");
 		
-		System.out.println("Digite a sua frase:");
-        String frase = consola.nextLine();
+		System.out.println("Insira uma frase:");
+        String frase2 = consola.nextLine().toLowerCase();
+        numVogais(frase2);
+        System.out.println("\n");*/
+		
+		System.out.println("Insira o número de segundos:");
+        int segundos = consola.nextInt();
+		convertSeconds(segundos);
 		consola.close();
 	}
 	
@@ -111,8 +121,47 @@ public class Ficha2
 		return count;
 	}
 	
-	public static int numVogais(String aFrase2) 
+	public static void numVogais(String aFrase2) 
 	{
-		return 0;
+		String exemploString = aFrase2;
+
+        int totalA = 0;
+        int totalE = 0;
+        int totalI = 0;
+        int totalO = 0;
+        int totalU = 0;
+        char ch;
+        for (int i = 0; i < exemploString.length(); i++) 
+        {
+            ch = exemploString.charAt(i);
+            if (ch == 'a')
+                totalA++;
+			if (ch == 'e')
+                totalE++;
+            if (ch == 'i')
+                totalI++;
+            if (ch == 'o')
+                totalO++;
+            if (ch == 'u')
+                totalU++;
+        }
+        System.out.println("a: " + totalA);
+        System.out.println("e: " + totalE);
+        System.out.println("i: " + totalI);
+        System.out.println("o: " + totalO);
+        System.out.println("u: " + totalU);
+	}
+	
+	public static void convertSeconds(int aSegundos) 
+	{
+		int segundos = aSegundos;
+		int minutos = segundos / 60;
+		int horas = segundos / 3600;
+		int dias = horas / 24;
+		
+		System.out.println("Dias: " + dias);
+        System.out.println("Horas: " + horas);
+        System.out.println("Minutos: " + minutos);
+        System.out.println("Segundos: " + segundos);
 	}
 }
